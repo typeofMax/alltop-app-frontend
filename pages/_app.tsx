0cb3a301e678dcc31c566174a1a5104e72f15186
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-22 23:50:51 +0300

    feat: Add head component and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css';
+//Libs
+import Head from 'next/head';
+//Types
 import type { AppProps } from 'next/app';
+//Styles
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+	return (
+		<>
+			<Head>
+				<title>AllTop - лучший топ</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
+};
 
 export default MyApp;
