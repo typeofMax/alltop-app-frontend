b415d00f39d0d07fb268ccfd8f078df497a7474f
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-22 23:49:20 +0300

    feat: Create custom document

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Html, Main, NextScript, Head, DocumentContext, DocumentInitialProps} from 'next/document';
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+
+	render(): JSX.Element {
+		return (
+			<Html lang='ru'>
+				<Head>
+					<link rel='preconnect' href='https://fonts.googleapis.com' />
+					<link rel='preconnect' href='https://fonts.gstatic.com' />
+					<link
+						href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@200;300;400;500;600;700&display=swap'
+						rel='stylesheet'
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
