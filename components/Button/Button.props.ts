53899719a3aa3d1197888b6d23ceec1438fa383b
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-23 10:42:01 +0300

    style: Add semi

--- components/Button/Button.props.ts
+++ components/Button/Button.props.ts
@@ -1,7 +1,7 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react';
 
 export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-	children: ReactNode,
-	arrow?: 'right' | 'down' | 'none',
-	appearance: 'primary' | 'ghost',
+	children: ReactNode;
+	arrow?: 'right' | 'down' | 'none';
+	appearance: 'primary' | 'ghost';
 }
