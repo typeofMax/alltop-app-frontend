f65cac0605eb461d174811033b281c1fbfbeeb1b
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-08-21 18:49:57 +0300

    feat: Create context

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -1,30 +1,33 @@
 //@Types
-import { FunctionComponent } from 'react';
+import { FC, FunctionComponent } from 'react';
 import { ILayoutProps } from './Layout.props';
 //@Components
 import { Footer } from './Footer/Footer';
 import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
+import { AppContextProvider, IAppContext } from '../core/context/app.context';
 //@Styles
 import s from './Layout.module.css';
 
-const Layout = ({ children }: ILayoutProps): JSX.Element => {
+const Layout: FC<ILayoutProps> = ({ children }) => {
 	return (
 		<div className={s.wrapper}>
-			<Header className={s.header}/>
-			<Sidebar className={s.sidebar}/>
-			<main className={s.body} >{children}</main>
-			<Footer className={s.footer}/>
+			<Header className={s.header} />
+			<Sidebar className={s.sidebar} />
+			<main className={s.body}>{children}</main>
+			<Footer className={s.footer} />
 		</div>
 	);
 };
 
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
 	return function withLayoutComponent(props: T): JSX.Element {
 		return (
-			<Layout>
-				<Component {...props} />
-			</Layout>
+			<AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+				<Layout>
+					<Component {...props} />
+				</Layout>
+			</AppContextProvider>
 		);
 	};
 };
