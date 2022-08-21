279c2f1122f072d0428035013fbefadd71ac11c9
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-08-21 18:48:19 +0300

    feat: Create menu array

---
+++ core/helpers/helpers.tsx
@@ -0,0 +1,15 @@
+//@Types
+import { IFirstLevelMenu } from '../interfaces/menu.interface';
+import { TopLevelCategory } from '../interfaces/page.interface';
+//@Images
+import CoursesIcon from './icons/courses.svg';
+import BooksIcon from './icons/books.svg';
+import ProductsIcon from './icons/goods.svg';
+import ServicesIcon from './icons/services.svg';
+
+export const firstLevelMenu: IFirstLevelMenu[] = [
+	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
+	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
+	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
+	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
+];
