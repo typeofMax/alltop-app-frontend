2ca3263680a0f15a29d398bc41db7bbe238a6c5c
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-08-27 23:42:37 +0300

    feat: Create salary parse regex

--- core/helpers/helpers.tsx
+++ core/helpers/helpers.tsx
@@ -8,8 +8,31 @@ import ProductsIcon from './icons/goods.svg';
 import ServicesIcon from './icons/services.svg';
 
 export const firstLevelMenu: IFirstLevelMenu[] = [
-	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
+	{
+		route: 'courses',
+		name: 'Курсы',
+		icon: <CoursesIcon />,
+		id: TopLevelCategory.Courses,
+	},
+	{
+		route: 'services',
+		name: 'Сервисы',
+		icon: <ServicesIcon />,
+		id: TopLevelCategory.Services,
+	},
+	{
+		route: 'books',
+		name: 'Книги',
+		icon: <BooksIcon />,
+		id: TopLevelCategory.Books,
+	},
+	{
+		route: 'products',
+		name: 'Продукты',
+		icon: <ProductsIcon />,
+		id: TopLevelCategory.Products,
+	},
 ];
+
+export const priceRu = (price: number): string =>
+	price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ').concat(' ₽');
