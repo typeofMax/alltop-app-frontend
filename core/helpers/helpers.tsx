c1d6aa418ea7f50680c7163d26bd0ff1b6e5d1ee
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-08-28 21:48:15 +0300

    feat: Create a function for inflecting words

--- core/helpers/helpers.tsx
+++ core/helpers/helpers.tsx
@@ -34,5 +34,24 @@ export const firstLevelMenu: IFirstLevelMenu[] = [
 	},
 ];
 
+//Деление числа на сотни, тысячи и т.д.
 export const priceRu = (price: number): string =>
-	price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ').concat(' ₽');
+	price
+		.toString()
+		.replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+		.concat(' ₽');
+
+//Склонение слов
+
+export const declOfNum = (
+	value: number,
+	titles: [string, string, string]
+): string => {
+	const cases = [2, 0, 1, 1, 1, 2];
+
+	return titles[
+		value % 100 > 4 && value % 100 < 20
+			? 2
+			: cases[value % 10 < 5 ? value % 10 : 5]
+	];
+};
