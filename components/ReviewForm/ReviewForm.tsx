ded1bc93cf0625dc7e98fe5e188d1f122c7d06c5
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-09-03 20:21:55 +0300

    feat: Create form validate

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -29,7 +29,7 @@ export const ReviewForm: FC<IReviewFormProps> = ({
 
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
-			<div className={cn(s.reviewForm, className)} {...props}>
+			<div className={cn(s.wrapper, className)} {...props}>
 				<Input
 					placeholder='Имя'
 					{...register('name', {
@@ -40,19 +40,24 @@ export const ReviewForm: FC<IReviewFormProps> = ({
 				<Input
 					placeholder='Заголовок отзыва'
 					className={s.title}
-					{...register('title')}
+					{...register('title', {
+						required: { value: true, message: 'Заполните заголовок' },
+					})}
+					error={errors.title}
 				/>
 				<div className={s.rating}>
 					<span>Оценка:</span>
 					<Controller
 						control={control}
 						name='rating'
+						rules={{ required: { value: true, message: 'Поставьте оценку' } }}
 						render={({ field }): JSX.Element => (
 							<Rating
 								rating={field.value}
 								setRating={field.onChange}
 								isEditable
 								ref={field.ref}
+								error={errors.rating}
 							/>
 						)}
 					/>
@@ -60,7 +65,10 @@ export const ReviewForm: FC<IReviewFormProps> = ({
 				<TextArea
 					placeholder='Текст отзыва'
 					className={s.description}
-					{...register('description')}
+					{...register('description', {
+						required: { value: true, message: 'Заполните текст' },
+					})}
+					error={errors.description}
 				/>
 				<div className={s.submit}>
 					<Button appearance='primary'>Отправить</Button>
