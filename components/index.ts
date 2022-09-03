746183ea76c87d4fc31ffe2bae544764ed5f88da
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-09-03 19:18:55 +0300

    feat: Create form

--- components/index.ts
+++ components/index.ts
@@ -9,6 +9,7 @@ export * from './Sort/Sort';
 export * from './Search/Search';
 export * from './Product/Product';
 export * from './Review/Review';
+export * from './ReviewForm/ReviewForm';
 export * from './Divider/Divider';
 export * from './ui/Button/Button';
 export * from './ui/Input/Input';
