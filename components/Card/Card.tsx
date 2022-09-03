071f4987359e0ce78a2e1aa6c683bf9d415bf97d
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-09-03 22:24:49 +0300

    feat: Add forward ref

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,20 +1,27 @@
+/* eslint-disable react/display-name */
 //@Libs
 import cn from 'classnames';
 //@Types
-import { FC } from 'react';
+import { ForwardedRef, forwardRef } from 'react';
 import { ICardProps } from './Card.props';
 //@Styles
 import s from './Card.module.css';
 
-export const Card: FC<ICardProps> = ({ children, color = 'white', className, ...props }) => {
-	return (
-		<div
-			className={cn(s.card, className, {
-				[s.blue]: color == 'blue',
-			})}
-			{...props}
-		>
-			{children}
-		</div>
-	);
-};
+export const Card = forwardRef(
+	(
+		{ children, color = 'white', className, ...props }: ICardProps,
+		ref: ForwardedRef<HTMLDivElement>
+	): JSX.Element => {
+		return (
+			<div
+				className={cn(s.card, className, {
+					[s.blue]: color == 'blue',
+				})}
+				{...props}
+				ref={ref}
+			>
+				{children}
+			</div>
+		);
+	}
+);
