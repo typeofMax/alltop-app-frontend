59a61bd92360a1acd8e0256ee79ae7cd69376909
typeofMax <s47813080@gmail.com>
2022-09-13 21:50:21 +0300

    feat: Change span to button

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -1,39 +1,36 @@
 //@Libs
 import cn from 'classnames';
 //@Types
-import { FC } from 'react';
+import { FC, KeyboardEvent } from 'react';
 import { ISortProps, SortEnum } from './Sort.props';
 //@Styles
 import s from './Sort.module.css';
 //@Images
 import SortIcon from './Sort.svg';
 
-export const Sort: FC<ISortProps> = ({
-	sort,
-	setSort,
-	className,
-	...props
-}) => {
+export const Sort: FC<ISortProps> = ({ sort, setSort, className, ...props }) => {
+
 	return (
 		<div className={cn(s.wrapper, className)} {...props}>
-			<span
+			<button
 				onClick={(): void => setSort(SortEnum.Rating)}
 				className={cn(s.type, {
 					[s.active]: sort == SortEnum.Rating,
 				})}
+
 			>
-				<SortIcon className={s.icon}/>
+				<SortIcon className={s.icon} />
 				По рейтингу
-			</span>
-			<span
+			</button>
+			<button
 				onClick={(): void => setSort(SortEnum.Price)}
 				className={cn(s.type, {
 					[s.active]: sort == SortEnum.Price,
 				})}
 			>
-				<SortIcon className={s.icon}/>
+				<SortIcon className={s.icon} />
 				По&nbsp;цене
-			</span>
+			</button>
 		</div>
 	);
 };
