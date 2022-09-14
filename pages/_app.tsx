15885ee72996e5afe35b2244285c92f7e390755e
typeofMax <s47813080@gmail.com>
2022-09-14 21:35:00 +0300

    feat: Add yandex metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,29 @@
 //Libs
 import Head from 'next/head';
+import ym from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
 //Types
 import type { AppProps } from 'next/app';
 //Styles
 import '../styles/globals.css';
+import { Router } from 'next/router';
+
+Router.events.on('routeChangeComplete', (url: string) => {
+	if (typeof window !== 'undefined') {
+		ym('hit', url);
+	}
+});
 
 const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
 	return (
 		<>
 			<Head>
-				<link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
-				<title>AllTop - лучший топ</title>
+				<title>MyTop - наш лучший топ</title>
+				<link rel='icon' href='/favicon.ico' />
 				<meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
-				<meta property='og:locale' content='ru_RU'/>
+				<meta property='og:locale' content='ru_RU' />
 			</Head>
+			<YMInitializer accounts={[]} options={{ webvisor: true, defer: true }} version='2' />
 			<Component {...pageProps} />
 		</>
 	);
