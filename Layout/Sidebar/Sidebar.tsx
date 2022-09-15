f360eb5e6b5bde7bf24f98efd1cf4f0248b07a94
typeofMax <s47813080@gmail.com>
2022-09-15 11:11:19 +0300

    refactor: Remove search

--- Layout/Sidebar/Sidebar.tsx
+++ Layout/Sidebar/Sidebar.tsx
@@ -5,7 +5,6 @@ import { FC } from 'react';
 import { ISidebarProps } from './Sidebar.props';
 //@Components
 import { Menu } from '../Menu/Menu';
-import { Search } from '../../components';
 //@Styles
 import s from './Sidebar.module.css';
 //@Images
@@ -16,7 +15,6 @@ export const Sidebar: FC<ISidebarProps> = ({ className, ...props }) => {
 	return (
 		<aside className={cn(className, s.wrapper)} {...props}>
 			<Logo className={s.logo} />
-			<Search />
 			<Menu />
 		</aside>
 	);
